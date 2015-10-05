055fb67c2e5a538a6ecd762f0598605eb40609f6
thebeanogamer <thebeanogamer@gmail.com>
2015-10-05 19:59:36 +0100

    Edited Variables
    Added while loop

--- Variables.py
+++ Variables.py
@@ -1,8 +1,9 @@
-name = input("Hi there. Whats your name? ")
-age = input("Hi " + name + ". How old are you? ")
-print ("Hi " + name + ". You are " + age + ".")
-correct = input("Is that right? ")
-if correct is ["yes", "Yes", "Yep", "yep", "correct", "Correct", "Right", "right", "true", "True"]:
-	print("Yay, I was right")
-else:
-	print("Awww, I got it wrong")
+while True:
+	name = input("Hi there. Whats your name? ")
+	age = input("Hi " + name + ". How old are you? ")
+	print ("Hi " + name + ". You are " + age + ".")
+	correct = input("Is that right? ")
+	if correct is ["yes", "Yes", "Yep", "yep", "correct", "Correct", "Right", "right", "true", "True"]:
+		print("Yay, I was right")
+	else:
+		print("Awww, I got it wrong")
