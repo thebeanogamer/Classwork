247635ab56ece716e6cab72a4937e928dfdeab8d
thebeanogamer <thebeanogamer@gmail.com>
2015-10-05 19:28:59 +0100

    Finished travel method check program

--- Flight Check.py
+++ Flight Check.py
@@ -1,10 +1,15 @@
-distance = int(input("How far is your journey?"))
+while True:
+	distance = int(input("How far is your journey in km?"))
 
-if distance <= 500 :
-	canFly = input("Can you/do you want to fly?")
-	if canFly == "Yes" or canFly == "yes":
-		print ("Take a plane!"
-	else
-		print ("Get on a boat or a coach!")
-else
-	print ("Drive your car or take the train!")
+	if distance >= 500 :
+		canFly = input("Can you/do you want to fly?")
+		if canFly == "Yes" or canFly == "yes":
+			print ("Take a plane!")
+		else:
+			print ("Get on a boat or a coach!")
+	else:
+		ownCar = input("Do you own a car?")
+		if ownCar == "yes" or ownCar == "Yes":
+			print ("Drive your car!")
+		else:
+			print ("Take the train")
