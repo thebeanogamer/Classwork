50fde0d61fc7be2ce1afa931abb5a38eaef0906c
thebeanogamer <thebeanogamer@gmail.com>
2015-10-05 19:50:33 +0100

    Edited Flight Check
    Added space shuttle

--- Flight Check.py
+++ Flight Check.py
@@ -1,12 +1,14 @@
 while True:
 	distance = int(input("How far is your journey in km?"))
 
-	if distance >= 500 :
+	if 40075 >= distance >= 500 :
 		canFly = input("Can you/do you want to fly?")
 		if canFly == "Yes" or canFly == "yes":
 			print ("Take a plane!")
 		else:
 			print ("Get on a boat or a coach!")
+	elif distance >= 40075:
+		print ("Hop on a space shuttle!")
 	else:
 		ownCar = input("Do you own a car?")
 		if ownCar == "yes" or ownCar == "Yes":
