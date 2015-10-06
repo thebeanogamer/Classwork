5359144acf54b222b546f67291e7436605c49846
thebeanogamer <thebeanogamer@gmail.com>
2015-10-06 11:01:22 +0100

    Added go counter

--- Guess The Random Number.py
+++ Guess The Random Number.py
@@ -2,16 +2,20 @@ import random
 import os
 
 while True:
+	times = 0
 	answer = random.randint(1,100)
 	print ("In this game you have to guess the whole number, between 1 and 100!")
 	while True:
 		guess = int(input("Guess the number: "))
 		if guess == answer:
-			print("You got it right")
+			times = times + 1
+			print("You got it right, and it took you ",times, "goes")
 			input("Hit enter to start again")
 			os.system("cls")
 			break
 		elif answer > guess:
+			times = times + 1
 			print("Too low, try again")
 		elif answer < guess:
+			times = times + 1
 			print ("Too high, try again")
