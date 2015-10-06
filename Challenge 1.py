f1ec15e435ced1d46f23ed0e371f47bdbd86a5a8
thebeanogamer <thebeanogamer@gmail.com>
2015-10-06 10:54:49 +0100

    Create Challenge 1.py

---
+++ Challenge 1.py
@@ -0,0 +1,22 @@
+A = 40
+B = 32
+# Assigns the variables
+print (A + B)
+# This prints 72 as they are intergers
+print (str(A) + str(B))
+# This prints 4032 as they are strings so python cannot interpret them
+
+number = int(input("Input a number: "))
+# Takes in a number to the variable "number"
+number = number * 3
+# Times the variable "number" by 3
+print ("That number times 3 is: ", number)
+# Prints out the variable "number" with a explination
+
+D = "7"
+E = "72"
+# Assigns the variables as strings
+print (D + E)
+# Prints out the variables
+print (str(D) + str(E))
+# Prints out the variables
