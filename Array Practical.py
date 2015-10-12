f3d13aa9848393da975ec6fd64b368630942993a
thebeanogamer <thebeanogamer@gmail.com>
2015-10-12 14:57:33 +0100

    Create Array Practical.py

---
+++ Array Practical.py
@@ -0,0 +1,9 @@
+Names = ["Fred","Sue","Daniel","James","Alex","Josh","Robert"]
+a = 0
+age = 0
+while a < len(Names):
+	print ("Hello", Names[a])
+	age = int(input("How old are you? "))
+	if age <= 16:
+		print ("You should still be at school!")
+	a = a + 1
