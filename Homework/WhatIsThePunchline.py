33fbade01001fdfca62b8dae693b898bad199907
thebeanogamer <thebeanogamer@gmail.com>
2015-10-23 13:57:38 +0100

    Create WhatIsThePunchline.py

---
+++ Homework/WhatIsThePunchline.py
@@ -0,0 +1,8 @@
+import random
+Jokes = ["Why did the chicken cross the road?", "What room has no doors", "What did the fish say when he bumped into the concrete wall?", "A man walked into a bar...", "Why did the poor man sell yeast?"]
+Punchlines = ["To get to the outher side", "A mushroom", "Damn", "He said oww!", "To raise some dough"]
+x = random.randint(1,5)
+print("Press enter for punchline")
+print (Jokes[x])
+input()
+print(Punchlines[x])
