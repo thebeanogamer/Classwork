4d3633af15416e422d29501a1e4eab770cae15b1
thebeanogamer <thebeanogamer@gmail.com>
2015-10-23 11:16:56 +0100

    Create Magic8Ball.py

---
+++ Homework/Magic8Ball.py
@@ -0,0 +1,4 @@
+import random
+answers = ["Possibly","Definatly Not","Uncertain","It is not clear","I think you are right","I think you are wrong!","It is likley","Unlikely"]
+input("What is your question? ")
+print (answers[random.randint(1,8)])
