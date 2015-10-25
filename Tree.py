8f9fc715f108041bbf65d5628ad95a822d971771
thebeanogamer <thebeanogamer@gmail.com>
2015-10-25 18:35:41 +0000

    Added Explanations To Tree

--- Tree.py
+++ Tree.py
@@ -9,13 +9,17 @@ while True:
 		height = int(height) + 1
 		#Converts height to a intger
 		if height > 3 and height <= 21:
+			#If the number is whithin the condintions requested then it prints the tree
 			while x < height:
 				print ("*" * x)
 				x = x + 1
 		else:
+			#If the user inputs a number, but not one requested it reiterates
 			print ("No, thats not what I asked for. Read it again!")
 			print ()
 	elif height == "exit":
+		#If the user types exit the program closes
 		sys.exit()
 	else:
+		#If the input is not a number it clarifies
 		print("Thats not a number, make sure you write it in digits!")
