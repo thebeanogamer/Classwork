4581403a7c133b7ba79caade4705befd586fef31
Daniel Milnes <thebeanogamer@gmail.com>
2016-03-16 13:13:58 +0000

    Added Explanations To Task 1

--- Coursework/Code1.py
+++ Coursework/Code1.py
@@ -6,6 +6,7 @@ continue1 = True
 
 def Roll(sides):
 	if sides == 4 or sides == 6 or sides == 12:
+		# Checks for valid input
 		number = random.randint(1,sides)
 		print ("I rolled a", sides, "sided dice, and got", number)
 	else:
@@ -14,9 +15,11 @@ def Roll(sides):
 while continue1 == True:
 	sides = input("Pick a 4,6 or 12 sided dice to roll: ")
 	if sides.isdigit() == True:
+		# Ensures the input is a number
 		sides = int(sides)
 		Roll(sides)
 		continue2 = (input("Would you like to continue: ").lower())
+		# Allows user to stop the program
 		os.system('cls')
 		if continue2 != "yes":
 			print ("Bye!")
