68524bb9221dd8da92c1d209b6f7b53da912527c
Daniel Milnes <thebeanogamer@gmail.com>
2016-09-19 09:54:46 +0100

    Built Initial Program
    Still needs validation

--- Coursework/A453/Code1.py
+++ Coursework/A453/Code1.py
@@ -1,1 +1,17 @@
+import os
 
+Continue = True
+while Continue == True:
+	x = 0
+	String = input("What is the string: ").lower()
+	String = String.split()
+	Term = input("What is the search term: ").lower()
+	while x < (len(String)):
+		if Term == String[x]:
+			print("Pos", (x + 1))
+		x = x + 1
+	ContinueRaw = input("Continue Y/N ").lower()
+	if ContinueRaw == "n":
+		Continue = False
+	else:
+		os.system('cls')
