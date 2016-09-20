ce7d046463f4cce37f869a7a130aa51b9a7dc464
Daniel Milnes <thebeanogamer@gmail.com>
2016-09-20 11:15:34 +0100

    Added Punctuation V1

--- Coursework/A453/Code1.py
+++ Coursework/A453/Code1.py
@@ -1,14 +1,30 @@
 import os
 
+Punctuation = ["!",'"',"£","$","%","^","&","*","(",")","-","=","_","+","{""}",":","[","]",";","@","'","~","#","<",">",",",".","?","/","¬","`","/","*","-","+"]
+
+def RemovePunctuation(Original):
+	y = 0
+	z = 0
+	Words = len(Original)
+	while y != Words:
+		while z != len(Punctuation):
+			Original[x].replace(Punctuation[y],"")
+			z = z + 1
+		y = y + 1
+		z = 0
+	y = y + 1
+
 Continue = True
 while Continue == True:
 	x = 0
-	String = input("What is the string: ").lower()
-	String = String.split()
-	Term = input("What is the search term: ").lower()
-	while x < (len(String)):
+	a = 0
+	String = input("What is the string: ").lower().split()
+	while a != len(String):#
+		
+	Term = input("What is the search term: ").lower().split()
+	while x != len(String):
 		if Term == String[x]:
-			print("Pos", (x + 1))
+			print("Word", (x + 1))
 		x = x + 1
 	ContinueRaw = input("Continue Y/N ").lower()
 	if ContinueRaw == "n":
