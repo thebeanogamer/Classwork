18b927d040dea04a7f5980f793f82fd36ab8680b
Daniel Milnes <thebeanogamer@gmail.com>
2016-09-26 09:49:23 +0100

    Added annotation and user interaction
    The program will now tell the user if the string never appears

--- Coursework/A453/Code1.py
+++ Coursework/A453/Code1.py
@@ -8,25 +8,51 @@ Continue = True
 # Causes the while loop to run
 
 while Continue == True:
+	# Ensures program will run until told otherwise
 	x = 0
-	a = 0
+	# Used to monitor position in string
+	Present = False
+	# Creates a failsafe incase the string is not present
 	String = input("What is the string: ").lower()
+	# Asks the user to input a string
 	no_punct1 = ""
 	no_punct2 = ""
+	# Creates empty variables to store punctuation free strings
 	for char in String:
+		# Repeats for every character in the string
 		if char not in punctuations:
+			# Only runs if the character is not punctiation
 			no_punct1 = no_punct1 + char
+			# Adds the character to the punctuation free string
 	no_punct1 = no_punct1.split()
+	# Breaks the punctuation free string for easier searching
 	Term = input("What is the search term: ").lower()
+	# Asks the user to input a search term
 	for char in Term:
+		# Repeats for every character in the term
 		if char not in punctuations:
+			# Only runs if the character is not punctiation
 			no_punct2 = no_punct2 + char
+			# Adds the character to the punctuation free string
 	while x != len(no_punct1):
+		# Runs for every word in the string
 		if no_punct2 == no_punct1[x]:
+			# Checks if the word is that which it is looking for
 			print("Word", (x + 1))
+			# Informs the user of its descovery
+			Present = True
+			# Saves that the string is present at least once
 		x = x + 1
+		# Causes the program to repeat
+	if Present == False:
+		# Checks if the term ever appeared
+		print ("Term not present!")
+		# Informs the user of its descovery
 	ContinueRaw = input("Continue Y/N ").lower()
+	# Asks the user if they want to repeat
 	if ContinueRaw == "n":
+		# If the user does not want to the loop will stop and the program will end
 		Continue = False
 	else:
 		os.system('cls')
+		# Clears the screen and starts the loop again
