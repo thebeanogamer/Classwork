e1f54edc5ad01f4205c52894a6f36e5aaf1dcc97
Daniel Milnes <thebeanogamer@gmail.com>
2016-09-26 09:34:42 +0100

    Fixed misc issues and added punctuation removal to search term

--- Coursework/A453/Code1.py
+++ Coursework/A453/Code1.py
@@ -1,20 +1,28 @@
 import os
+# Imports OS to make terminal clear possible
 
 punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+# Creates list of all punctuation to replace
 
 Continue = True
+# Causes the while loop to run
+
 while Continue == True:
 	x = 0
 	a = 0
 	String = input("What is the string: ").lower()
 	no_punct1 = ""
+	no_punct2 = ""
 	for char in String:
 		if char not in punctuations:
 			no_punct1 = no_punct1 + char
 	no_punct1 = no_punct1.split()
 	Term = input("What is the search term: ").lower()
+	for char in Term:
+		if char not in punctuations:
+			no_punct2 = no_punct2 + char
 	while x != len(no_punct1):
-		if Term == no_punct1[x]:
+		if no_punct2 == no_punct1[x]:
 			print("Word", (x + 1))
 		x = x + 1
 	ContinueRaw = input("Continue Y/N ").lower()
