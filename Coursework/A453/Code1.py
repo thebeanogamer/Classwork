37d6f200991a737a9e67b2a9388a7e0e8dbe723e
Daniel Milnes <thebeanogamer@gmail.com>
2016-09-27 10:52:37 +0100

    Fixed Spelling Issues

--- Coursework/A453/Code1.py
+++ Coursework/A453/Code1.py
@@ -12,7 +12,7 @@ while Continue == True:
 	x = 0
 	# Used to monitor position in string
 	Present = False
-	# Creates a failsafe incase the string is not present
+	# Creates a failsafe i ncase the string is not present
 	String = input("What is the string: ").lower()
 	# Asks the user to input a string
 	no_punct1 = ""
@@ -21,7 +21,7 @@ while Continue == True:
 	for char in String:
 		# Repeats for every character in the string
 		if char not in punctuations:
-			# Only runs if the character is not punctiation
+			# Only runs if the character is not punctuation
 			no_punct1 = no_punct1 + char
 			# Adds the character to the punctuation free string
 	no_punct1 = no_punct1.split()
@@ -31,7 +31,7 @@ while Continue == True:
 	for char in Term:
 		# Repeats for every character in the term
 		if char not in punctuations:
-			# Only runs if the character is not punctiation
+			# Only runs if the character is not punctuation
 			no_punct2 = no_punct2 + char
 			# Adds the character to the punctuation free string
 	while x != len(no_punct1):
@@ -39,7 +39,7 @@ while Continue == True:
 		if no_punct2 == no_punct1[x]:
 			# Checks if the word is that which it is looking for
 			print("Word", (x + 1))
-			# Informs the user of its descovery
+			# Informs the user of its discovery
 			Present = True
 			# Saves that the string is present at least once
 		x = x + 1
@@ -47,7 +47,7 @@ while Continue == True:
 	if Present == False:
 		# Checks if the term ever appeared
 		print ("Term not present!")
-		# Informs the user of its descovery
+		# Informs the user of its discovery
 	ContinueRaw = input("Continue Y/N ").lower()
 	# Asks the user if they want to repeat
 	if ContinueRaw == "n":
