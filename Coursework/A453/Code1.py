7d59accf62d9d9b2fc80eb32f4ebbac6a5a8ccd5
Daniel Milnes <thebeanogamer@gmail.com>
2016-10-04 10:33:14 +0100

    Added checks for if the strings are empty

--- Coursework/A453/Code1.py
+++ Coursework/A453/Code1.py
@@ -12,26 +12,34 @@ while Continue == True:
 	x = 0
 	# Used to monitor position in string
 	Present = False
-	# Creates a failsafe in case the string is not present
+	# Creates a failsafe incase the string is not present
 	String = input("What is the string: ").lower()
 	# Asks the user to input a string
+	while String == "":
+		print ("Um, no. Give me somthing to work with!")
+		String = input("What is the string: ").lower()
+		# Repeats the question until it gets an answer
 	no_punct1 = ""
 	no_punct2 = ""
 	# Creates empty variables to store punctuation free strings
 	for char in String:
 		# Repeats for every character in the string
 		if char not in punctuations:
-			# Only runs if the character is not punctuation
+			# Only runs if the character is not punctiation
 			no_punct1 = no_punct1 + char
 			# Adds the character to the punctuation free string
 	no_punct1 = no_punct1.split()
 	# Breaks the punctuation free string for easier searching
 	Term = input("What is the search term: ").lower()
+	while Term == "":
+		print ("Um, no. Give me somthing to work with!")
+		Term = input("What is the search term: ").lower()
+		# Repeats the question until it gets an answer
 	# Asks the user to input a search term
 	for char in Term:
 		# Repeats for every character in the term
 		if char not in punctuations:
-			# Only runs if the character is not punctuation
+			# Only runs if the character is not punctiation
 			no_punct2 = no_punct2 + char
 			# Adds the character to the punctuation free string
 	while x != len(no_punct1):
@@ -39,7 +47,7 @@ while Continue == True:
 		if no_punct2 == no_punct1[x]:
 			# Checks if the word is that which it is looking for
 			print("Word", (x + 1))
-			# Informs the user of its discovery
+			# Informs the user of its descovery
 			Present = True
 			# Saves that the string is present at least once
 		x = x + 1
@@ -47,7 +55,7 @@ while Continue == True:
 	if Present == False:
 		# Checks if the term ever appeared
 		print ("Term not present!")
-		# Informs the user of its discovery
+		# Informs the user of its descovery
 	ContinueRaw = input("Continue Y/N ").lower()
 	# Asks the user if they want to repeat
 	if ContinueRaw == "n":
