fdedc5ad5cdf138559ac8712429267ea6d5b4e86
Daniel Milnes <thebeanogamer@gmail.com>
2018-12-12 11:05:59 +0000

    Write Stack program

---
+++ Python/Stack.py
@@ -0,0 +1,10 @@
+array = []
+while True:
+    action = input("What action would you like to take? ").lower()
+    if (action == "view"):
+        print (array)
+    elif (action == "add"):
+        array.append(input("What would you like to add? "))
+    elif (action == "remove"):
+        if (array != []):
+            array.pop()
