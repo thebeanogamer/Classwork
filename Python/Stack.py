24a5064dab5df1154971df47d4bc165cc75e64d8
Daniel Milnes <thebeanogamer@gmail.com>
2018-12-12 11:07:16 +0000

    Make Stack PEP8 compliant

--- Python/Stack.py
+++ Python/Stack.py
@@ -2,7 +2,7 @@ array = []
 while True:
     action = input("What action would you like to take? ").lower()
     if (action == "view"):
-        print (array)
+        print(array)
     elif (action == "add"):
         array.append(input("What would you like to add? "))
     elif (action == "remove"):
